f48b2ba2d5b4f54d3500ce4999ac3f74ca4662e9
ben-mojow <2601577+Grimsly@users.noreply.github.com>
2024-09-29 07:45:22 -0600

    Documentation: encryption comments

--- client/src/util/encryption.ts
+++ client/src/util/encryption.ts
@@ -10,8 +10,8 @@ const algorithm = "AES-CBC";
  * @returns The encrypted data in hex format
  */
 export function encrypt(password: string, salt: string, data: string) {
+  // Both IV and key have to be 32 bytes for AES-256
   const iv = random.getBytesSync(32).toString();
-
   const key = pkcs5.pbkdf2(password, salt, 2, 32);
   const aes_cipher = cipher.createCipher(algorithm, key);
 
@@ -21,6 +21,7 @@ export function encrypt(password: string, salt: string, data: string) {
 
   var encrypted = aes_cipher.output;
 
+  // Create a pill with the salt, IV, and the encrypted bytes
   return salt + iv + encrypted.getBytes();
 }
 
@@ -31,8 +32,10 @@ export function encrypt(password: string, salt: string, data: string) {
  * @returns The decrypted data string
  */
 export function decrypt(password: string, pill: string) {
+  // Knowing that the salt and IV are 32 bytes each, slice the pill to retrieve them
   const salt = pill.slice(0, 32);
   const iv = pill.slice(32, 64);
+  // The leftover pill will be the encrypted data in bytes form
   const encrypted_data = pill.slice(64);
   const key = pkcs5.pbkdf2(password, salt, 2, 32);
 
